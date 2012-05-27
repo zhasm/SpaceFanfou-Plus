ad61efc9cbb908be3b47a88996c81e24492f310b
Fangzhou Li <iruif.cn@gmail.com>
2012-05-27 19:54:07 +0800

    更新 "经典 logo" 插件

--- src/plugins/logo_remove_beta.js
+++ src/plugins/logo_remove_beta.js
@@ -2,6 +2,8 @@ SF.pl.logo_remove_beta = new SF.plugin((function($) {
 	var $header = $('.global-header-content');
 	if (! $header.length) return;
 
+	$('#sf_style_logo_remove_beta').remove();
+
 	var origin = $header.css('background-image');
 	var beta = 'url(http://static2.fanfou.com/img/fanfou_beta.png)';
 
