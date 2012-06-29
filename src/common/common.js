93440bd08dfd27eac86f8eb5d9f679570d42b735
Fangzhou Li <iruif.cn@gmail.com>
2012-06-29 13:34:24 +0800

    提示使用 Chrome 17 及以下版本的用户升级浏览器

--- src/common/common.js
+++ src/common/common.js
@@ -1,4 +1,21 @@
 (function($) {
+	
+	/* 判断 Chrome 版本号 */
+	
+	(function() {
+		var ua = navigator.userAgent.toLowerCase();
+		var re = /.*chrome\/([\w.]+).*/;
+		var full_version = re.exec(ua)[1];
+		var main_version = parseInt(full_version, 10);
+		var key = '__sf_version_check__';
+		if (main_version < 18 && localStorage[key] === undefined) {
+			localStorage[key] = true;
+			alert('您正在使用较旧版本的 Chrome 浏览器 (' +
+				full_version + 
+				'). 太空饭否++ 以后的版本将停止提供对 17 及以下版本 Chrome 的支持. ' + 
+				'强烈建议您立即升级到最新版本.');
+		}
+	})();
 
 	/* 输入框自动获得焦点 */
 	
