bb4b1714e4a678ae917e09928fa262f01b0d7f71
Fangzhou Li <iruif.cn@gmail.com>
2012-07-13 02:37:37 +0800

    更新设置页脚本

--- src/options.js
+++ src/options.js
@@ -10,7 +10,7 @@ document.addEventListener('DOMContentLoaded', function() {
 		if ($elem.type == 'checkbox')
 			return $elem.checked;
 		else
-			return eval($elem.value);
+			return JSON.parse($elem.value);
 	}
 	function setValue($elem, value) {
 		if ($elem.type == 'checkbox') {
@@ -100,4 +100,61 @@ document.addEventListener('DOMContentLoaded', function() {
 		$screenshot.style.left = targetX + 'px';
 		$screenshot.style.top = targetY + 'px';
 	}
+}, false);
+
+function current(target, self) {
+	localStorage['latest_options_tab'] = self.id;
+	var button = self.parentElement;
+	if (button.classList.contains('current')) return;
+	var current_button = $$('li.current a')[0];
+	var current_tab_no = current_button ?
+		+current_button.id.split('').reverse()[0] : $$('#navigation ul li').length;
+	var target_tab_no = self.id.split('').reverse()[0];
+	var tab = $$('#navigation li');
+	var i, j, len = tab.length;
+	for (i = 0; i < len; i++) {
+		tab[i].classList.remove('current');
+	}
+	button.classList.add('current');
+	for (i = 0; j = $('tabs' + i); i++) {
+		j.style.display = 'none';
+		j.style.webkitAnimation = j.style.animation = '';
+	}
+	var target_style = $(target).style;
+	target_style.display = 'block';
+	var animation_name = current_tab_no > target_tab_no ? 'leftSlideIn' : 'rightSlideIn';
+	target_style.webkitAnimation = animation_name + '.2s ease-out';
+
+	var ul = $$('#' + target + ' ul')[0];
+	if (! ul) return;
+	clearTimeout(ul.timeout);
+	ul.style.overflow = 'hidden';
+	ul.timeout = setTimeout(function() {
+		ul.style.overflow = '';
+	}, 250);
+
+	forEach($$('button'), function(btn) {
+		clearTimeout(btn.timeout);
+		btn.style.webkitAnimation = 'btn-fadeIn .6s ease-in';
+		btn.timeout = setTimeout(function() {
+			btn.style.webkitAnimation = '';
+		}, 600);
+	});
+}
+
+addEventListener('load', function load(e) {
+	forEach($$('#navigation a'), function(nav_link, i) {
+		nav_link.onclick = function(e) {
+			current('tabs' + i, this);
+		}
+	});
+
+	setTimeout(function() {
+		if (document.documentElement.clientHeight < 300) return load();
+		forEach($$('div[id^="tabs"]'), function(tab) {
+			tab.style.visibility = 'visible';
+		});
+		var latest_tab = localStorage['latest_options_tab'] || 'tab1';
+		SF.fn.emulateClick($(latest_tab));
+	}, 16);
 }, false);
