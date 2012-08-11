915f8c241913056abd8e1c62f7b435f5115d3c9c
Fangzhou Li <iruif.cn@gmail.com>
2012-08-12 00:50:20 +0800

    细节更新

--- src/common/style.js
+++ src/common/style.js
@@ -35,57 +35,81 @@
 	}, function() {
 		var $, $totop;
 		var hidden = false;
+
 		function hideTotop() {
 			if (! hidden && $totop) {
-				$totop.hide();
+				$totop.css({
+					visibility: 'hidden',
+					opacity: 0,
+					cursor: 'default',
+					'-webkit-transition': ''
+				});
 				hidden = true;
 			}
 		}
+
+		function showTotop() {
+			if (hidden && $totop) {
+				$totop.css({
+					visibility: 'visible',
+					opacity: .5,
+					cursor: 'pointer',
+					'-webkit-transition': 'opacity .4s ease-in-out'
+				});
+				hidden = false;
+			}
+		}
+
 		var totop = $i('pagination-totop');
 		if (! totop) {
 			var pagination = $cn(document, 'pagination')[0];
+
 			if (! pagination) {
 				pagination = $c('div');
 				pagination.className = 'pagination';
 				pagination.style = 'display: none;';
+
 				if ($i('content')) {
 					$i('content').appendChild(pagination);
 				} else if ($i('stream')) {
 					$i('stream').parentNode.appendChild(pagination);
 				}
 			}
+
 			totop = $c('a');
 			totop.id = 'pagination-totop';
-			totop.href = '#';
 			totop.innerHTML = '返回顶部';
+
 			pagination.appendChild(totop);
 		}
+
+		totop.removeAttribute('href');
 		totop.addEventListener('click', function(e) {
 			hideTotop();
 			SF.fn.goTop(e);
 		}, false);
+
 		SF.fn.waitFor(function() {
 			return $ = window.jQuery;
 		}, function() {
 			var $win = $(window);
 			var main_top = 66;
+
 			$totop = $(totop);
-			$totop.hide();
 			$totop.removeClass('more more-right');
-			$totop.css('visibility', 'visible');
-			$win.scroll((function() {
-				var onscroll = SF.fn.throttle(function() {
-					hidden = false;
-					if ($win.scrollTop() < main_top)
-						$totop.fadeOut();
-					else
-						$totop.fadeIn();
-				}, 500);
-				return function() {
+			hideTotop();
+
+			var onscroll = SF.fn.throttle(function() {
+				if ($win.scrollTop() < main_top)
 					hideTotop();
-					onscroll();
-				}
-			})());
+				else
+					showTotop();
+			}, 500);
+
+			$win.scroll(function() {
+				hideTotop();
+				onscroll();
+			});
 		});
 	});
 
@@ -94,14 +118,16 @@
 			$cn(document, 'upload-close-handle').length;
 	}, function() {
 		var upload_button = $i('upload-button');
+
 		$i('upload-file').addEventListener('change', function(e) {
 			upload_button.classList[this.files.length ? 'add' : 'remove']('file-chosen');
 		}, false);
+
 		$cn(document, 'upload-close-handle')[0].addEventListener('click', function(e) {
 			upload_button.classList.remove('file-chosen');
 		}, false);
 	});
-	
+
 	SF.fn.waitFor(function() {
 		return $i('timeline-notification') && $i('timeline-count');
 	}, function() {
@@ -113,10 +139,12 @@
 		var timeout;
 		var input_tags = ['textarea', 'input', 'TEXTAREA', 'INPUT'];
 		var waiting;
+
 		function doCount(x) {
 			count += x;
 			btn.dataset.pullProgress = count
 		}
+
 		addEventListener('mousewheel', function(e) {
 			if (waiting) return;
 			if (body.scrollTop) return;
@@ -124,10 +152,13 @@
 			if (input_tags.indexOf(e.target.tagName) > -1) return;
 			if (notif.style.display == 'none') return;
 			if (! parseInt(unread_count.textContent)) return;
+
 			clearTimeout(timeout);
 			doCount(1);
+
 			if (count === 3) {
 				count = 0;
+
 				waiting = setTimeout(function() {
 					waiting = null;
 					SF.fn.emulateClick(btn, true);
@@ -135,10 +166,13 @@
 				}, 250);
 			} else {
 				notif.classList.add('onpull');
+
 				timeout = setTimeout(function recover() {
 					count = Math.min(3, count);
 					count = Math.max(1, count);
+
 					doCount(-1);
+
 					if (! count) {
 						notif.classList.remove('onpull');
 					} else {
