cae7dfad2183b481c648f66083340636eb4d1576
Fangzhou Li <iruif.cn@gmail.com>
2012-09-04 10:20:31 +0800

    清理代码

--- src/plugins/advanced_sidebar.js
+++ src/plugins/advanced_sidebar.js
@@ -11,121 +11,132 @@ SF.pl.advanced_sidebar = new SF.plugin((function($) {
 		'load': function() {
 			SF.cb.advanced_sidebar = function(data) {
 				SF.cb.advanced_sidebar = undefined;
+
 				var created = new Date(data.created_at);
 				var bgImage = data.profile_background_image_url;
+
 				var regDuration = Math.floor((new Date() - created) /
 											 (1000 * 3600 * 24));
 				var regYear = Math.floor(regDuration / 365.2425);
 				var regMonth = Math.floor(
 					(regDuration - regYear * 365.2425) / 30.4369);
+
 				var duration = (regYear > 0 || regMonth > 0 ? '约 ' : '') +
 					(regYear > 0 ? regYear + ' 年' +
 					 (regMonth > 0 ? '零 ' + regMonth + ' 个月' : '') :
 					 (regMonth > 0 ? regMonth + ' 个月' :
 					  (regDuration >= 7 ? '不足一个月' :
 					   (regDuration > 0 ? '不足一周' : '刚来不到一天'))));
+
 				// 从饭否诞生开始计算
 				var sinceFanfouStart = Math.round(
 						(new Date() - new Date(2007, 4, 12)) /
 						(1000 * 3600 * 24))
+
 				var statusFreq =
 					(data.statuses_count /
 					 (regDuration -
 					  (created < new Date(2009, 6, 8) ? 505 : 0)))
 					 .toFixed(2);
+
 				if (!isFinite(statusFreq))
 					statusFreq = data.statuses_count;
+
 				// 饭粒公式
 				var actIndex = ((40 * statusFreq) - (statusFreq * statusFreq)) / 400;
 				if (statusFreq > 20)
 					actIndex = 1;
 				if (data.protected)
 					actIndex = actIndex * 0.75
-				var infIndex =
-					((10 *(Math.sqrt(data.followers_count)) / Math.log(regDuration + 100)) +
-					 ((data.followers_count / 100) + (regDuration / 100)) * actIndex)
-					  .toFixed(0);
-				$insert
-				.after(
+
+				var infIndex = (
+					(10 *(Math.sqrt(data.followers_count)) / Math.log(regDuration + 100)) +
+					((data.followers_count / 100) + (regDuration / 100)) * actIndex
+				).toFixed(0);
+
+				$insert.after(
 					$('<div />').addClass('stabs advanced_group')
-					);
+				);
+
 				$('.advanced_group')
-				.append(
-					$('<h2 />').addClass('advanced_title')
-					.text('统计信息')
+					.append(
+						$('<h2 />').addClass('advanced_title')
+							.text('统计信息')
 					)
-				.append($('<ul />'));
+					.append($('<ul />'));
+
 				$('.advanced_group>ul')
-				.append(
-					$('<li />').addClass('advanced ptest')
-					.text('注册于 ' + SF.fn.formatDate(created))
-					)
-				.append(
-					$('<li />').addClass('advanced')
-					.text('饭龄：' + duration +
-						' (' + regDuration + ' 天)')
-					.prop('title', '注册时长')
 					.append(
-						$('<div />')
-						.addClass('statbar statbar_a')
-						.append(
-							$('<div />').each(function() {
-								var $elem = $(this);
-								setTimeout(function() {
-									$elem.width(regDuration / sinceFanfouStart * 100 + '%');
-								}, 0);
-							})
-					   )
-						)
+						$('<li />').addClass('advanced ptest')
+							.text('注册于 ' + SF.fn.formatDate(created))
 					)
-				.append(
-					$('<li />')
-					.addClass('advanced')
-					.text('饭量：平均 ' + statusFreq + ' 条消息 / 天')
-					.prop('title', '消息频率')
 					.append(
-						$('<div />')
-						.addClass('statbar statbar_b')
+						$('<li />').addClass('advanced')
+						.text('饭龄：' + duration +
+							' (' + regDuration + ' 天)')
+						.prop('title', '注册时长')
 						.append(
-							$('<div />').each(function() {
-								var $elem = $(this);
-								setTimeout(function() {
-									$elem.width(Math.min(100, statusFreq) + '%');
-								}, 0);
-							})
+							$('<div />')
+							.addClass('statbar statbar_a')
+							.append(
+								$('<div />').each(function() {
+									var $elem = $(this);
+									setTimeout(function() {
+										$elem.width(regDuration / sinceFanfouStart * 100 + '%');
+									}, 0);
+								})
 							)
 						)
 					)
-				.append(
-					$('<li />').addClass('advanced')
-					.text('饭粒：' + infIndex + ' 个')
-					.prop('title', '影响力')
 					.append(
-						$('<div />')
-						.addClass('statbar statbar_c')
+						$('<li />')
+							.addClass('advanced')
+							.text('饭量：平均 ' + statusFreq + ' 条消息 / 天')
+							.prop('title', '消息频率')
+							.append(
+								$('<div />')
+									.addClass('statbar statbar_b')
+									.append(
+										$('<div />').each(function() {
+											var $elem = $(this);
+											setTimeout(function() {
+												$elem.width(Math.min(100, statusFreq) + '%');
+											}, 0);
+										})
+									)
+							)
+					)
+					.append(
+						$('<li />').addClass('advanced')
+						.text('饭粒：' + infIndex + ' 个')
+						.prop('title', '影响力')
 						.append(
-							$('<div />').each(function() {
-								var $elem = $(this);
-								setTimeout(function() {
-									$elem.width(Math.min(100, infIndex / 3) + '%');
-								}, 0);
+							$('<div />')
+							.addClass('statbar statbar_c')
+							.append(
+								$('<div />').each(function() {
+									var $elem = $(this);
+									setTimeout(function() {
+										$elem.width(Math.min(100, infIndex / 3) + '%');
+									}, 0);
 								})
 							)
 						)
 					);
-				$('.ptest').addClass(
-						data.protected ? 'protected' : 'notprotected');
+
+				$('.ptest').addClass(data.protected ?
+					'protected' : 'notprotected');
 
 				var bgImageStyle = $('body').css('background-image');
 				if (bgImageStyle != 'none' && bgImage) {
 					$('<li />').addClass('advanced')
-					.append(
-						$('<a />')
-						.attr('href', bgImage)
-						.addClass('more')
-						.text('» 查看背景图片')
-					)
-					.appendTo('.advanced_group ul');
+						.append(
+							$('<a />')
+								.attr('href', bgImage)
+								.addClass('more')
+								.text('» 查看背景图片')
+						)
+						.appendTo('.advanced_group ul');
 				}
 			};
 			$script = $('<script />').attr('src',
