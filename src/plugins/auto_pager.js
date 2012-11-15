b9408128c6a98b4f287c38d3060b19124527e1d9
Lacc Riophae <iruif.cn@gmail.com>
2012-11-15 17:56:34 +0800

    更新 "自动翻页" 插件

--- src/plugins/auto_pager.js
+++ src/plugins/auto_pager.js
@@ -2,75 +2,84 @@ SF.pl.auto_pager = new SF.plugin((function($) {
 	var browse = 'http://fanfou.com/browse';
 	if (location.href.indexOf(browse) === 0)
 		return;
-  var $more = $('#pagination-more');
-  if (! $more.length) return;
+	var $more = $('#pagination-more');
+	if (! $more.length) return;
 
-  var is_loaded;
+	var is_loaded;
 
-  var $win = $(window);
-  var $html = $('html');
-  var body = document.body;
-  var docelem = document.documentElement;
-  var $totop = $('#pagination-totop');
+	var $win = $(window);
+	var $html = $('html');
+	var body = document.body;
+	var docelem = document.documentElement;
+	var $totop = $('#pagination-totop');
 
-  var currentPos;
-  var remain = 500;
+	var current_pos;
+	var mousewheel_down = false;
+	var remain = 500;
 
-  var onScroll = SF.fn.throttle(autoPager, 250);
+	var onScroll = SF.fn.throttle(autoPager, 250);
 
-  function autoPager() {
-    currentPos = body.scrollTop + docelem.clientHeight;
-    if (currentPos <= $more.offset().top - remain)
-      return;
-    if (currentPos >= body.clientHeight)
-      return;
-    if ($more.hasClass('loading'))
-      return;
+	function autoPager() {
+		if (! mousewheel_down) 
+			return; 
+		current_pos = body.scrollTop + docelem.clientHeight;
+		if (current_pos <= $more.offset().top - remain)
+			return;
+		if (current_pos >= body.clientHeight)
+			return;
+		if ($more.hasClass('loading'))
+			return;
 
 		SF.fn.emulateClick($more[0]);
-  }
+	}
+	
+	function onMousewheel(e) {
+		mousewheel_down = e.originalEvent.wheelDeltaY < 0;
+	}
 
-  function onKeypress(e) {
-    if (e.target !== body) return;
-    if (e.ctrlKey || e.altKey) return;
-    var key = e.which;
-    switch (key) {
-      case 97: case 116:
-        e.preventDefault();
-        break;
-    }
-    switch (key) {
-      case 116:
+	function onKeypress(e) {
+		if (e.target !== body) return;
+		if (e.ctrlKey || e.altKey) return;
+		var key = e.which;
+		switch (key) {
+			case 97: case 116:
+				e.preventDefault();
+				break;
+		}
+		switch (key) {
+			case 116:
 				SF.fn.emulateClick($totop[0]);
-        break;
-      case 97:
-        is_loaded ? unload() : load();
-        break;
-    }
-  }
+				break;
+			case 97:
+				is_loaded ? unload() : load();
+				break;
+		}
+	}
 
-  function load() {
-    if (is_loaded) return;
-    is_loaded = true;
+	function load() {
+		if (is_loaded) return;
+		is_loaded = true;
 
-    $win.bind('scroll', onScroll);
-  }
+		$win.bind('scroll', onScroll);
+	}
 
-  function unload() {
-    if (! is_loaded) return;
-    is_loaded = false;
+	function unload() {
+		if (! is_loaded) return;
+		is_loaded = false;
 
-    $win.unbind('scroll', onScroll);
-  }
+		$win.unbind('scroll', onScroll);
+	}
 
-  return {
-    'load': function() {
-      load();
-      $win.bind('keypress', onKeypress);
-    },
-    'unload': function() {
-      unload();
-      $win.unbind('keypress', onKeypress);
-    }
-  };
+	return {
+		'load': function() {
+			load();
+			$win.bind('keypress', onKeypress);
+			$win.bind('mousewheel', onMousewheel);
+		},
+		'unload': function() {
+			unload();
+			$win.unbind('keypress', onKeypress);
+			$win.unbind('mousewheel', onMousewheel);
+		}
+	};
 })(jQuery));
